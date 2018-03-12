870816f84d699aa0f6c71ff2352577c8e133a142
FantoM <freereg@ukr.net>
2018-03-13 00:12:27 +0200

    wifi

--- app/src/main/java/tm/fantom/simplemqtt/util/PrefStorage.java
+++ app/src/main/java/tm/fantom/simplemqtt/util/PrefStorage.java
@@ -3,10 +3,6 @@ package tm.fantom.simplemqtt.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-/**
- * Created by nsfantom on 1/3/18.
- */
-
 public final class PrefStorage {
     private final String TOKEN = "token";
     private final String COUNTRY_CODE = "country_code";
